f09980b18babfa5a0ccdee71be04125785c0b9ff
yoshikimasubuchi <yoshikimasubuchi@chao-labnoMacBook-puro.local>
2018-07-04 21:10:40 +0900

    tnp

--- ruby/180704/import_recommend.rb
+++ ruby/180704/import_recommend.rb
@@ -1,14 +1,12 @@
 require 'csv'
+require 'pry'
 
 female_20_idx, male_20_idx, female_30_idx, male_30_idx = 2, 8, 14, 20
 segment_female_20, segment_male_20, segment_female_30, segment_male_30 = [19, 20, 21], [10, 11, 12], [22, 23], [13, 14]
 recommend_instanse = {}
+blank_rows = [7, 13, 19]
 
 CSV.foreach('180628_nanaco.csv', headers: false).with_index do |row, ln|
-  begin
-  next if ln < 2
-  next unless row[3].nil?
-
   case ln
   when female_20_idx
     p recommend_instanse
@@ -26,7 +24,4 @@ CSV.foreach('180628_nanaco.csv', headers: false).with_index do |row, ln|
   end
   shop = { name: row[3], shop_comment: row[5], tabelog_url: row[6], image_urls: [row[7], row[8], row[9]] }
   recommend_instanse[:shops] << shop
-  rescue => e
-
-  end
 end
